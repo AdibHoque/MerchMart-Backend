b34c4f02a575bba13a665ae0bec67a5361c869ba
AdibHoque <adibhoque04@gmail.com>
2024-08-17 13:51:28 +0600

    Connected the express server with MongoDb

--- index.js
+++ index.js
@@ -14,3 +14,32 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Listening to Port: ", port)
 })
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.knvnnno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // // Connect the client to the server(optional starting in v4.7)
+    // await client.connect();
+    const Collection = client.db('MerchMart').collection('Products');
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
